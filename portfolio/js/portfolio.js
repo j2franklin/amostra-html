6fa61d4a51f5e3ee523e7186a22571d21d947971
Jaison <kammer.black@gmail.com>
2026-03-19 23:50:38 -0300

    Retirei imagens do livro "Contar estrelas do céu..." e mudei a extensão da capa para JPG

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -69,7 +69,7 @@ const portfolio = {
         {
             title: "Contar estrelas do no céu da rugas no dedo?",
             folder: "img/editorial/contar-estrelas-no-ceu/",
-            cover: "cover.jpg",
+            cover: "cover.JPG",
             images: [
                 "exposition.jpg",
                 "folha-rosto.jpg",
